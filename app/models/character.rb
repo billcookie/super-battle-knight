b79027c12d0217e217dbadef82e67e1766741e24
Bill Cook <billcook8122@gmail.com>
2022-08-13 16:50:50 +0900

    active storage for heroku added

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,7 @@
 class Character < ApplicationRecord
-  has_one_attached :photo
   has_many :battles, through: :obligations
   has_many :obligations
+  has_one_attached :photo
   validates :name, :age, :ranking, presence: true
   validates :name, uniqueness: true
 end
