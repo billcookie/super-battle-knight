7e4f5b46e5c35038cf60ea3c2c5f702231fe8162
Bill Cook <billcook8122@gmail.com>
2022-08-14 14:42:37 +0900

    Map added with geocode search

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -4,6 +4,9 @@
 
 import { application } from "./application"
 
+import GetAllAddressesController from "./get_all_addresses_controller"
+application.register("get-all-addresses", GetAllAddressesController)
+
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
 
